b6ed84707cbfd33673f68d2c4df346ad7b56bbac
Ace <matt.aceves@yahoo.com>
2024-12-11 23:04:03 -0700

    fixed truck class

--- Develop/src/classes/Truck.ts
+++ Develop/src/classes/Truck.ts
@@ -42,10 +42,10 @@ class Truck extends Vehicle implements AbleToTow {
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
     // TODO: Get the make an model of the vehicle if it exists
-    const vehicleMake = this.make;
-    const vehicleModel = this.model;
+    const vehicleMake = vehicle.make;
+    const vehicleModel = vehicle.model;
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
-    if (this.weight <= this.towingCapacity) {
+    if (vehicle.weight <= this.towingCapacity) {
     // TODO: If it is, log that the vehicle is being towed
     console.log(`${vehicleMake} ${vehicleModel} is being towed`);
     }
