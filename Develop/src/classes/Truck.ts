f4917ca48b2b4cee6267e932a432b51a3965df4d
Ace <matt.aceves@yahoo.com>
2024-12-11 21:38:43 -0700

    fixed truck class

--- Develop/src/classes/Truck.ts
+++ Develop/src/classes/Truck.ts
@@ -61,15 +61,26 @@ class Truck extends Vehicle implements AbleToTow {
     super.printDetails();
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
-    console.log(`VIN: ${this.vin}
-      Make: ${this.make}
-      Model: ${this.model}
-      Year: ${this.year}
-      Weight: ${this.weight} lbs
-      Top Speed: ${this.topSpeed} mph
-      Color: ${this.color}
-      Towing Capacity: ${this.towingCapacity} lbs
-      Wheels: ${this.wheels}`);
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Towing Capacity: ${this.towingCapacity} lbs`);
+    console.log(
+      `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 3: ${this.wheels[2].getDiameter} inch with a ${this.wheels[2].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 4: ${this.wheels[3].getDiameter} inch with a ${this.wheels[3].getTireBrand} tire`
+    );
 }
 };
 // Export the Truck class as the default export
